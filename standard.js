e255d94b36040d6e715289458a95f61d9254d01c
fentonseel <73243759+fentonseel@users.noreply.github.com>
2021-11-22 15:16:17 +0000

    Made the website responsive
    Made the page responsive and will adapt to the size of the screen.
    A burger menu will be added when necessary.
    Changed some minor formatting for the footer.

--- standard.js
+++ standard.js
@@ -1,1 +1,8 @@
-//javascript file for website
+function burgerMenuClick()
+{
+    let navi = document.getElementById("burgerMenuSelection");
+
+    navi.classList.toggle("hide");
+
+    console.log(navi.classList);
+}
